b4907a8f8b696514dd3c241aa9f3a8a06ae26f10
Tomas Roy <hello@minicreative.net>
2019-04-24 18:38:15 -0400

    sample layers added, testing basic hover functionality

--- main.js
+++ main.js
@@ -14,14 +14,25 @@ function initializePrimaryMap () {
 	// Position map for Vieques
 	primaryMap.setView([18.14, -65.43], 12);
 
-	// Add base layer
-	L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	// Add layers
+	var layerA = L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(primaryMap);
+	var layerB = L.tileLayer( 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(primaryMap);
+	var layerC = L.tileLayer( 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(primaryMap);
 
-	// Add other layers
-	L.tileLayer( 'https://tile.waymarkedtrails.org/hiking/${z}/${x}/${y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(primaryMap);
-	
+	// Find hover IDs
+	$("#a").mouseenter(function () {
+		layerA.bringToFront();
+	});
+	$("#b").mouseenter(function () {
+		layerB.bringToFront();
+	});
+	$("#c").mouseenter(function () {
+		layerC.bringToFront();
+	});
 }
