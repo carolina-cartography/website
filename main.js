83b79ccbc22435330c12c10cad0d81c98a595e66
Javier A Arce-Nazario <jarce@live.unc.edu>
2019-04-25 10:22:05 -0400

    changed basemaps and added contour layer

--- main.js
+++ main.js
@@ -15,17 +15,25 @@ function initializePrimaryMap () {
 	primaryMap.setView([18.14, -65.43], 12);
 
 	// Add layers
-	var layerA = L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	var layerA = L.tileLayer( 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(primaryMap);
-	var layerB = L.tileLayer( 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(primaryMap);
-	var layerC = L.tileLayer( 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(primaryMap);
+	var layerB = L.tileLayer( 'https://cartocollective.blob.core.windows.net/vieques/1977/vieques1977/{z}/{x}/{y}.png',
+		{tms: true}
+	).addTo(primaryMap);
+	var layerC = L.tileLayer( 'https://cartocollective.blob.core.windows.net/vieques/1983/{z}/{x}/{y}.png',
+	{tms: true}
+).addTo(primaryMap);
+	var layer1 = L.tileLayer( 'https://cartocollective.blob.core.windows.net/vieques/rasters/contour/{z}/{x}/{y}.png',
+	{tms: true}
+)	.addTo(primaryMap);
 
-	// Find hover IDs
+	layer1.bringToFront();
+// TO DO
+// Ideally on layer is shown instead of all layers being called.
+// Hopefully that will make it faster 
+
+// Find hover IDs
 	$("#a").mouseenter(function () {
 		layerA.bringToFront();
 	});
